2b1c29ce12e9623e137b70c1a9473d75dc0584cd
ShawnShoper <xiehao3692@vip.qq.com>
2018-12-28 22:07:25 +0800

    add grant page

--- src/main/java/org/shieldproject/usercenter/config/OAuth2ApprovalController.java
+++ src/main/java/org/shieldproject/usercenter/config/OAuth2ApprovalController.java
@@ -1,19 +1,11 @@
 package org.shieldproject.usercenter.config;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
-
-import javax.servlet.http.HttpServletRequest;
-import java.util.Map;
-
-@Controller
-@SessionAttributes("authorizationRequest")
+//@Controller
+//@SessionAttributes("authorizationRequest")
 public class OAuth2ApprovalController {
 
-    @RequestMapping("/oauth/confirm_access")
-    public String getAccessConfirmation(Map<String, Object> model, HttpServletRequest request)
-            throws Exception {
+//    @RequestMapping("/oauth/confirm_access")
+    public String getAccessConfirmation() {
 
         return "oauth-grant";
     }
