203e1f971dccc4146f059beebfd0d375c4580db5
ShawnShoper <xiehao3692@vip.qq.com>
2019-01-02 16:01:35 +0800

    add register

---
+++ crawler-platform/crawler-provider/src/main/java/org/shieldproject/skynet/crawler/provider/api/CrawlerApi.java
@@ -0,0 +1,8 @@
+package org.shieldproject.skynet.crawler.provider.api;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CrawlerApi {
+
+}
---
+++ crawler-platform/crawler-provider/src/main/java/org/shieldproject/skynet/crawler/provider/api/HealthApi.java
@@ -0,0 +1,7 @@
+package org.shieldproject.skynet.crawler.provider.api;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthApi {
+}
