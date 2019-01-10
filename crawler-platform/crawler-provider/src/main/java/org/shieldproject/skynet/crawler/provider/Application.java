7dec05558adef268e28e89741d5866d9567a6ea3
ShawnShoper <xiehao3692@vip.qq.com>
2019-01-11 00:20:26 +0800

    aaaa

--- crawler-platform/crawler-provider/src/main/java/org/shieldproject/skynet/crawler/provider/Application.java
+++ crawler-platform/crawler-provider/src/main/java/org/shieldproject/skynet/crawler/provider/Application.java
@@ -1,13 +1,24 @@
 package org.shieldproject.skynet.crawler.provider;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.Arrays;
+import java.util.List;
 
 @SpringBootApplication
 @EnableScheduling
 public class Application {
+    //    public static void main(String[] args) {
+//        SpringApplication.run(Application.class, args);
+//    }
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        List<String> region = Arrays.asList("成都市", "自贡市");
+        String address = "成都市武侯区武侯大道";
+        String tmp = null;
+        for (String re : region)
+            if ((tmp = address.replace(re, "")).length() != address.length() || (tmp = address.replace(re.substring(0, re.length() - 1), "")).length() != address.length())
+                break;
+        System.out.println(tmp);
     }
 }
--- user-center/src/main/java/org/shieldproject/usercenter/config/OAuth2ApprovalController.java
+++ user-center/src/main/java/org/shieldproject/usercenter/config/OAuth2ApprovalController.java
@@ -1,12 +1,17 @@
 package org.shieldproject.usercenter.config;
 
-//@Controller
-//@SessionAttributes("authorizationRequest")
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@SessionAttributes("authorizationRequest")
 public class OAuth2ApprovalController {
-
-//    @RequestMapping("/oauth/confirm_access")
-    public String getAccessConfirmation() {
-
+    @RequestMapping("/oauth/confirm_access")
+    public String getAccessConfirmation(HttpServletRequest request) {
+        System.out.println(request);
         return "oauth-grant";
     }
 
